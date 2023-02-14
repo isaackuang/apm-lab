7b52a49c15f2ca39ae41de98e463c26c63c46c72
Isaac Tseng <isaac.kuang@gmail.com>
2023-02-14 13:10:49 +0800

    add java

--- express-web/src/app.js
+++ express-web/src/app.js
@@ -1,39 +1,13 @@
+/*app.js*/
+const express = require("express");
 
-console.log(process.env.APM_ENABLE)
+const PORT = parseInt(process.env.PORT || "3000");
+const app = express();
 
-if (process.env.APM_ENABLE == 'true') {
-    console.log("Enable tracing")
-    require('./tracing');
-}
-
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.get('/health', function (req, res) {
-    res.send('Health!');
+app.listen(PORT, () => {
+  console.log(`Listening for requests on http://localhost:${PORT}`);
 });
-
-app.get('/sleep', function (req, res){
-  setTimeout(() => {
-    console.log("sleep")
-    res.send("I'm wake.")
-  }, 1000)
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-process.on('SIGTERM', shutDown);
-process.on('SIGINT', shutDown);
-
-function shutDown() {
-  console.log('Received kill signal, shutting down gracefully');
-  setTimeout(() => {
-      process.exit(0);
-  }, 120000);
-}
--- express-web/src/tracing.js
+++ express-web/src/tracing.js
@@ -1,50 +1,44 @@
-'use strict'
-
-const process = require('process');
-const opentelemetry = require('@opentelemetry/sdk-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { CollectorTraceExporter } =  require('@opentelemetry/exporter-collector-grpc');
-const { Metadata } = require("@grpc/grpc-js");
-const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+"use strict";
 
 const apmConfig = {
-  apiKey: `ApiKey ${process.env.APM_API_KEY}`,
   url: process.env.APM_URL,
   serviceName: process.env.APM_SERVICE_NAME,
   podId: process.env.POD_NAME
 }
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const opentelemetry = require("@opentelemetry/sdk-node");
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-http");
 
-const metadata = new Metadata()
-metadata.set('Authorization', apmConfig.apiKey);
-
-const collectorOptions = {
-  // url is optional and can be omitted - default is grpc://localhost:4317
-  url: apmConfig.url,
-  metadata
-};
-// configure the SDK to export telemetry data to the console
-// enable all auto-instrumentations from the meta package
-const traceExporter = new CollectorTraceExporter(collectorOptions);
 const sdk = new opentelemetry.NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: apmConfig.serviceName,
-    [SemanticResourceAttributes.K8S_POD_NAME]: apmConfig.podId
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    url: apmConfig.url + "/v1/traces",
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
   }),
-  traceExporter,
-  instrumentations: [getNodeAutoInstrumentations()]
+  resource: new Resource({
+    [ SemanticResourceAttributes.SERVICE_NAME ]: apmConfig.serviceName,
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
 });
 
-// initialize the SDK and register with the OpenTelemetry API
-// this enables the API to record telemetry
-sdk.start()
-  .then(() => console.log('Tracing initialized'))
-  .catch((error) => console.log('Error initializing tracing', error));
+sdk
+    .start()
+    .then(() => {
+        console.log("Tracing initialized");
+    })
+    .catch((error) => console.log("Error initializing tracing", error));
 
-// gracefully shut down the SDK on process exit
-process.on('SIGTERM', () => {
-  sdk.shutdown()
-    .then(() => console.log('Tracing terminated'))
-    .catch((error) => console.log('Error terminating tracing', error))
-    .finally(() => process.exit(0));
+process.on("SIGTERM", () => {
+    sdk
+        .shutdown()
+        .then(() => console.log("Tracing terminated"))
+        .catch((error) => console.log("Error terminating tracing", error))
+        .finally(() => process.exit(0));
 });
